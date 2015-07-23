2e8daac9d069301cd426ce3c3cc3546221f62c0d
piskun <piskun@corp.sputnik.ru>
2015-07-23 18:46:41 +0300

    base logic

---
+++ tictoctimer.go
@@ -0,0 +1,20 @@
+package tictoctimer
+
+import "time"
+
+type TicTocTimer struct {
+	duration time.Duration
+	start    time.Time
+}
+
+func (self *TicTocTimer) Tic() {
+	self.start = time.Now()
+}
+
+func (self *TicTocTimer) Toc() {
+	self.duration = time.Now().Sub(self.start)
+}
+
+func (self *TicTocTimer) TicToc() time.Duration {
+	return self.duration
+}
